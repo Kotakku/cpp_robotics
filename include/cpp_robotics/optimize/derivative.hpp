f77cf9e88118f87782877f66f4547b31d136567b
Kotakku <kotakkucu@gmail.com>
2022-09-28 18:15:20 +0900

    R^n -> R^mの関数の数値微分を追加

--- include/cpp_robotics/optimize/derivative.hpp
+++ include/cpp_robotics/optimize/derivative.hpp
@@ -43,6 +43,25 @@ static Eigen::VectorXd derivative(std::function<double(Eigen::VectorXd)> f, Eige
     return der;
 }
 
+// R^n -> R^mの数値微分
+static Eigen::MatrixXd derivative(std::function<Eigen::VectorXd(Eigen::VectorXd)> f, Eigen::VectorXd x, double eps = std::pow(std::numeric_limits<double>::epsilon(), 0.5))
+{
+    Eigen::MatrixXd der;
+    for(Eigen::VectorXd::Index i = 0; i < x.rows(); i++)
+    {
+        Eigen::VectorXd diff = Eigen::VectorXd::Zero(x.rows());
+        diff(i) = eps;
+        // 中央差分
+        Eigen::VectorXd der_col = ( f(x + diff) - f(x - diff) ) / (2.0*eps);
+        if(i == 0)
+        {
+            der = Eigen::MatrixXd::Zero(der_col.size(), x.rows());
+        }
+        der.col(i) = der_col;
+    }
+    return der;
+}
+
 /**
  * @brief R -> Rの2回数値微分
  * 
