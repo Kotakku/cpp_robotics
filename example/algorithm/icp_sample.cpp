8d916a8096fba1dbba41c70c6d63e7728d635050
Kotakku <kotakkucu@gmail.com>
2022-10-08 16:51:38 +0900

    example修正

--- example/algorithm/icp_sample.cpp
+++ example/algorithm/icp_sample.cpp
@@ -1,13 +1,16 @@
 #define CR_USE_MATPLOTLIB
 #include <iostream>
+#include <iomanip>
 #include <cpp_robotics/algorithm/algorithm.hpp>
 #include <cpp_robotics/vector/vector2.hpp>
 #include <cpp_robotics/third_party/matplotlib-cpp/matplotlibcpp.h>
+#include <Eigen/Geometry> 
 
 int main()
 {
     namespace cr = cpp_robotics;
 
+    std::cout << std::fixed << std::setprecision(3);
     {
         std::vector<Eigen::Vector2d> fixed = {
             {0,9},
@@ -23,12 +26,13 @@ int main()
         };
 
         std::vector<Eigen::Vector2d> p2 = fixed;
+        Eigen::Rotation2Dd t(M_PI/3);
+        t.toRotationMatrix();
         for(size_t i = 0; i < fixed.size(); i++)
         {
-            // p2[i][0] = fixed[i][1] + 1;
-            // p2[i][1] = -fixed[i][0];
-            p2[i][0] += 3.0; //fixed[i][0];
-            p2[i][1] += 1.0; //fixed[i][1];
+            p2[i][0] += 3.0;
+            p2[i][1] += 1.0;
+            p2[i] = t*p2[i];
         }
         auto [R, T] = cr::calcu_transformatoin<2>(p2, fixed);
 
@@ -36,6 +40,8 @@ int main()
         std::cout << R << std::endl;
         std::cout << "T = " << std::endl;
         std::cout << T << std::endl;
+        std::cout << "t = " << std::endl;
+        std::cout << t.matrix() << std::endl;
 
         std::cout << "///// check /////" << std::endl;
 
@@ -49,23 +55,40 @@ int main()
 
     {
         std::vector<Eigen::Vector2d> p1 = {
-            {0,8},
-            {0,7},
-            {0,5},
-            {0,3},
+            {6,9},
+            {5,8},
+            {5,7},
+            {5,6},
+            {5,5},
+            {5,4},
+            {1,3},
+            {1,2},
             {2,1},
+            {3,1},
             {4,1},
+            {5,1},
             {6,1},
+            {7,1},
             {8,3},
+            {8,4},
             {8,5},
+            {8,6},
+            {8,7},
+            {8,8},
             {8,9},
         };
 
         std::vector<Eigen::Vector2d> p2 = p1;
-        for(auto &v : p2)
-            v += Eigen::Vector2d(3, 0);
+        Eigen::Rotation2Dd t(M_PI/3);
+        t.toRotationMatrix();
+        for(size_t i = 0; i < p2.size(); i++)
+        {
+            p2[i][0] += 3.0;
+            p2[i][1] += 1.0;
+            p2[i] = t*p2[i];
+        }
 
-        auto [points, cnt] = cr::icp<2>(p2, p1, 50);
+        auto [points, H, cnt] = cr::icp<2>(p2, p1, 1e-4, 50);
 
         auto gen = [](std::vector<Eigen::Vector2d> ps)
         {
@@ -80,10 +103,32 @@ int main()
             return std::tuple<std::vector<double>, std::vector<double>>(x_, y_);
         };
 
+        auto gen_tf = [&](std::vector<Eigen::Vector2d> ps, Eigen::MatrixXd H)
+        {
+            std::vector<double> x_(ps.size()), y_(ps.size());
+
+            for(size_t i = 0; i < (ps.size()); i++)
+            {
+                auto p = cr::transform_by_homogeneous(H, ps[i]);
+                x_[i] = p[0];
+                y_[i] = p[1];
+            }
+
+            return std::tuple<std::vector<double>, std::vector<double>>(x_, y_);
+        };
+
+        std::cout << "H" << std::endl;
+        std::cout << H << std::endl;
+
         auto [x, y] = gen(p1);
-        matplotlibcpp::plot(x, y);
-        auto [x2, y2] = gen(points);
-        matplotlibcpp::plot(x2, y2);
+        matplotlibcpp::plot(x, y, "o--");
+        auto [x2, y2] = gen(p2);
+        matplotlibcpp::plot(x2, y2, "o--");
+        auto [xr, yr] = gen(points);
+        matplotlibcpp::plot(xr, yr, "o--");
+        
+        auto [xp, yp] = gen_tf(p2, H);
+        matplotlibcpp::plot(xp, yp); // 一致しない？
         matplotlibcpp::show();
 
     }
---
+++ example/filter/lpf.cpp
@@ -0,0 +1,24 @@
+#define CR_USE_MATPLOTLIB
+#include <iostream>
+#include <cpp_robotics/system/time_responce.hpp>
+#include <cpp_robotics/filter/low_pass_filter.hpp>
+#include <cpp_robotics/third_party/matplotlib-cpp/matplotlibcpp.h>
+
+int main()
+{
+    namespace cr = cpp_robotics;
+
+    const double dt = 1e-3;
+    const double f = 1; // [Hz]
+    const double omega = 2*M_PI*f; // [rad/s]
+    // const double tau = 1/omega; //[s]
+    cr::LowPassFilter lpf(omega, dt);
+
+    const double gain = 1.0;
+    auto [t, res] = cr::step([&](double u){return lpf.filtering(u); }, dt, 3.0);
+
+    namespace plt = matplotlibcpp;
+    plt::plot(t, res);
+
+    plt::show();
+}
