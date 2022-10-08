5bf7c4a6f8ce30d49619c96633f14f800fce8ae2
Kotakku <kotakkucu@gmail.com>
2022-10-08 23:35:48 +0900

    LPFのプログラム例を修正

--- example/filter/lpf_step.cpp
+++ example/filter/lpf_step.cpp
@@ -11,8 +11,8 @@ int main()
     const double dt = 1e-3;
     const double f = 1; // [Hz]
     const double omega = 2*M_PI*f; // [rad/s]
-    // const double tau = 1/omega; //[s]
-    cr::LowPassFilter lpf(omega, dt);
+    const double tau = 1/omega; //[s]
+    cr::LowPassFilter lpf(tau, dt);
 
     const double gain = 1.0;
     auto [t, res] = cr::step([&](double u){return lpf.filtering(u); }, dt, 3.0);
