70bc8c9f05f8050be692e35c062f02210400b088
Kotakku <kotakkucu@gmail.com>
2022-10-09 15:22:40 +0900

    フィルタのexample追加

--- example/filter/hpf_step.cpp
+++ example/filter/hpf_step.cpp
@@ -1,7 +1,7 @@
 #define CR_USE_MATPLOTLIB
 #include <iostream>
 #include <cpp_robotics/system/time_responce.hpp>
-#include <cpp_robotics/filter/low_pass_filter.hpp>
+#include <cpp_robotics/filter/high_pass_filter.hpp>
 #include <cpp_robotics/third_party/matplotlib-cpp/matplotlibcpp.h>
 
 int main()
@@ -11,10 +11,13 @@ int main()
     const double dt = 1e-3;
     const double f = 1; // [Hz]
     const double omega = 2*M_PI*f; // [rad/s]
-    cr::LowPassFilter lpf(omega, dt);
+    cr::HighPassFilter lpf(omega, dt);
+
+    const double gain = 1.0;
+    auto [t, res] = cr::step(lpf, 2.0);
 
     namespace plt = matplotlibcpp;
-    auto [t, res] = cr::step(lpf, 2.0);
     plt::plot(t, res);
+
     plt::show();
 }
