4e879a30cb3e1432b7903fcfb6d5b180a35c1f26
Kotakku <kotakkucu@gmail.com>
2022-10-10 00:51:28 +0900

    PIDvsNCTFのexampleを更新

--- example/controller/pid_vs_nctf.cpp
+++ example/controller/pid_vs_nctf.cpp
@@ -4,29 +4,27 @@
 #include <cpp_robotics/motor/dc_motor_list.hpp>
 #include <cpp_robotics/motor/motor_tf.hpp>
 
-static std::tuple<std::vector<double>, std::vector<double>, std::vector<double>, std::vector<double>, std::vector<double>> step_test(cpp_robotics::SisoFeedbackSystem &sys, double time, double dt)
+static std::tuple<std::vector<double>, std::vector<double>> ref_trajectory(double time, double dt)
 {
     std::vector<double> t = cpp_robotics::arrange(0, time, dt);
     std::vector<double> target(t.size());
-    std::vector<double> res(t.size());
-    std::vector<double> dres(t.size());
-    std::vector<double> input(t.size());
 
-    cpp_robotics::Differentiator diff_(1/dt, dt);
-    
-
-    for(size_t i = 0; i < res.size(); i++)
+    target[0] = 0;
+    for(size_t i = 1; i < target.size(); i++)
     {
-        target[i] = (res.size()/2 < i ) ? ( (res.size()*3/4 < i ) ? 0: 20 ) : 20*std::cos(i*0.03); // : 1.0;
-        res[i] = sys.control(target[i]);
-        dres[i] = diff_.filtering(res[i]);
-        input[i] = sys.u();
+        if(0.75*target.size() < i)
+            target[i] = 20*std::cos(i*3.5*dt);
+        else if(0.5*target.size() < i)
+            target[i] = ( (i < 0.625*target.size()) ? 10 : 20 );
+        else if(0.25*target.size() < i)
+            target[i] = 0;
+        else 
+            target[i] = -15 + 30 * ((double)(i) / (0.25*target.size()));
     }
-
-    return {t, target, input, res, dres};
+    return {t, target};
 }
 
-void make_nct_test(std::function<double(double)> f)
+void plot_nct_test(std::function<double(double)> f)
 {
     std::vector<double> e = cpp_robotics::arrange(-10, 10, 0.05);
     std::vector<double> e_dot(e.size());
@@ -44,86 +42,70 @@ void make_nct_test(std::function<double(double)> f)
 
 int main()
 {
-    using namespace cpp_robotics;
+    namespace cr = cpp_robotics;
+    namespace plt = matplotlibcpp;
+    using namespace cr::unit;
 
-    const double dt = 0.01;
+    const double dt = 0.001;
     const double max_output = 24;
 
     //////////////////// PID ////////////////////
-    PIDController::pid_param_t pid_param = 
+    cr::PIDController::pid_param_t pid_param = 
     {
         .Ts = dt,
-        .gpd = 100,
-        .Kp = 10.0,
+        .gpd = 100*2*M_PI,
+        .Kp = 6.0,
         .Ki = 0.0,
-        .Kd = 0.09,
+        .Kd = 0.2,
         .output_limit = std::pair(-max_output, max_output)
     };
-    PIDController pid(pid_param);
+    cr::PIDController pid(pid_param);
 
     //////////////////// NCTF ////////////////////
-    PIDController::pid_param_t nctf_pid_param = 
+    cr::PIDController::pid_param_t nctf_pid_param = 
     {
         .Ts = dt,
-        .gpd = 500,
-        .Kp = 0.8,
-        .Ki = 0.0,
-        .Kd = 0.005,
+        .gpd = 100*2*M_PI,
+        .Kp = 0.5,
+        .Ki = 0,
+        .Kd = 0,
         .output_limit = std::pair(-max_output, max_output)
     };
     
     const double nct_max_velocity = 300.0;
-    const double nct_slope = 10;
-    NctfController nctf(nct_max_velocity, nct_slope, nctf_pid_param, dt);
+    const double nct_slope = 25;
+    cr::NctfController nctf(nct_max_velocity, nct_slope, nctf_pid_param, dt);
 
     //////////////////// Plant ////////////////////
-    auto motor = DCGearedMotorParam(24_V, RZ_735VA_9517, IG42C_17);
-    auto plant = make_geared_motor_pos_tf(motor, dt); // [rad]
+    auto motor = cr::DCGearedMotorParam(24_V, cr::RZ_735VA_9517, cr::IG42C_17);
+    auto plant = make_geared_motor_pos_tf(motor, dt); // [V] -> [rad]
 
-    // make_nct_test(nctf.nct());
-    // {
-    //     auto vel_plant = make_geared_motor_vel_tf(motor, dt); // [rad]
-    //     namespace plt = matplotlibcpp;
-    //     auto [t, res] = step(vel_plant, 3.0, max_output);
-    //     plt::named_plot("responce", t, res);
-    //     plt::legend();
-    //     plt::show();
-    // }
+    // plot_nct_test(nctf.nct());
 
+    //////////////////// Simulation ////////////////////
+    auto [t, ref_trajecoty] = ref_trajectory(5.0, dt);
+    plt::named_plot("ref trajecoty", t, ref_trajecoty, "--");
+
+    // PID controller system simulation
     {
-        namespace plt = matplotlibcpp;
-        {
-            SisoFeedbackSystem siso_sys = cpp_robotics::make_feedback_system(pid, plant);
-            siso_sys.reset();
-            auto [t, target, input, ret, dres] = step_test(siso_sys, 5.0, dt);
-
-            plt::named_plot("target", t, target);
-            plt::named_plot("PID responce", t, ret);
-        }
-
-        {
-            SisoFeedbackSystem siso_sys = cpp_robotics::make_feedback_system(nctf, plant);
-            auto [t, target, input, ret, dres] = step_test(siso_sys, 5.0, dt);
-            plant.set_state_zero();
-            siso_sys.reset();
-            // plt::named_plot("target", t, target);
-            plt::named_plot("NCTF responce", t, ret);
-            // plt::named_plot("input", t, input);
-        }
-
-        // plt::named_plot("input", t, input);
-        // plt::named_plot("d responce / dt", t, dres);
-
-        plt::ylim(-30.0, 30.0);
-        plt::legend();
-        plt::show();
-
-        // // show NCT
-        // plt::named_plot("actual NCT", nctf.e_, nctf.e_dot_);
-        // plt::named_plot("target NCT", nctf.e_, nctf.target_e_dot_);
-        // plt::show();
+        cr::SisoFeedbackSystem siso_sys = cr::make_feedback_system(pid, plant);
+        siso_sys.reset();
+        auto [t, responce] = cr::lsim(siso_sys, ref_trajecoty);
+        plt::named_plot("PID responce", t, responce);
     }
 
+    // NCTF constoller system simulation
+    {
+        cr::SisoFeedbackSystem siso_sys = cr::make_feedback_system(nctf, plant);
+        siso_sys.reset();
+        auto [t, responce] = cr::lsim(siso_sys, ref_trajecoty);
+        plt::named_plot("NCTF responce", t, responce);
+    }
+
+    // show
+    plt::ylim(-30.0, 30.0);
+    plt::legend();
+    plt::show();
 }
 
 
