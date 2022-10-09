0390302fb3b3fad78d140e31a11b564564e4e048
Kotakku <kotakkucu@gmail.com>
2022-10-09 17:06:39 +0900

    impluse, step, lsimの柔軟性向上

--- include/cpp_robotics/system/time_responce.hpp
+++ include/cpp_robotics/system/time_responce.hpp
@@ -1,32 +1,81 @@
 #pragma once
 
 #include <tuple>
+#include <type_traits>
 #include "../utility/space.hpp"
 #include "./transfer_function.hpp"
 #include "./discrete_transfer_function.hpp"
 
-
 namespace cpp_robotics
 {
+
+/// @cond
+namespace internal
+{
+template <class T>
+class is_system_class
+{
+    template <class U>
+    static auto check(U&& x)->decltype(x.Ts(), x.responce(0.0), std::true_type{});
+
+    template <class U>
+    static auto check(...)->std::false_type;
+
+public:
+    static constexpr bool value = decltype(check<T>(std::declval<T>()))::value;
+};
+
+template <class T>
+class is_filter_class
+{
+    template <class U>
+    static auto check(U&& x)->decltype(x.Ts(), x.filtering(0.0), std::true_type{});
+
+    template <class U>
+    static auto check(...)->std::false_type;
+
+public:
+    static constexpr bool value = decltype(check<T>(std::declval<T>()))::value;
+};
+
+template <class T>
+struct is_pure_filter_class
+{
+    static constexpr bool value = not is_system_class<T>::value && is_filter_class<T>::value;
+};
+
+}
+/// @endcond
 
 /**
  * @brief インパルス応答を求める
  * 
  * @param sys 
+ * @param dt
  * @param time 
  * @return std::tuple<std::vector<double>, std::vector<double>> 
  */
-static std::tuple<std::vector<double>, std::vector<double>> impluse(TransferFunction &sys, double time)
+static std::tuple<std::vector<double>, std::vector<double>> impluse(const std::function<double(double)> &sys, double dt, double time)
 {
-    std::vector<double> t = arrange(0, time, sys.Ts());
+    std::vector<double> t = arrange(0, time, dt);
     std::vector<double> res(t.size());
-
     for(size_t i = 0; i < res.size(); i++)
     {
-        res[i] = sys.responce( i == 0 ? 1.0/sys.Ts() : 0.0);
+        res[i] = sys( i == 0 ? 1.0/dt : 0.0);
     }
-
     return {t, res};
+}
+
+template<class SysType, typename std::enable_if<internal::is_system_class<SysType>::value>::type* = nullptr>
+static std::tuple<std::vector<double>, std::vector<double>> impluse(SysType &sys, double time)
+{
+    return impluse([&](double u){ return sys.responce(u); }, sys.Ts(), time);
+}
+
+template<class SysType, typename std::enable_if<internal::is_pure_filter_class<SysType>::value>::type* = nullptr>
+static std::tuple<std::vector<double>, std::vector<double>> impluse(SysType &sys, double time)
+{
+    return impluse([&](double u){ return sys.filtering(u); }, sys.Ts(), time);
 }
 
 /**
@@ -51,50 +100,48 @@ static std::tuple<std::vector<double>, std::vector<double>> step(const std::func
     return {t, res};
 }
 
-/**
- * @brief ステップ応答を求める
- * 
- * @param sys 
- * @param time 
- * @param gain 
- * @return std::tuple<std::vector<double>, std::vector<double>> 
- */
-static std::tuple<std::vector<double>, std::vector<double>> step(TransferFunction &sys, double time, const double gain = 1.0)
+template<class SysType, typename std::enable_if<internal::is_system_class<SysType>::value>::type* = nullptr>
+static std::tuple<std::vector<double>, std::vector<double>> step(SysType &sys, double time, const double gain = 1.0)
 {
     return step([&](double u){ return sys.responce(u); }, sys.Ts(), time, gain);
 }
 
-/**
- * @brief ステップ応答を求める
- * 
- * @param sys 
- * @param time 
- * @param gain 
- * @return std::tuple<std::vector<double>, std::vector<double>> 
- */
-static std::tuple<std::vector<double>, std::vector<double>> step(DiscreteTransferFunction &sys, double time, const double gain = 1.0)
+template<class FilterType, typename std::enable_if<internal::is_pure_filter_class<FilterType>::value>::type* = nullptr>
+static std::tuple<std::vector<double>, std::vector<double>> step(FilterType &filter, double time, const double gain = 1.0)
 {
-    return step([&](double u){ return sys.responce(u); }, sys.Ts(), time, gain);
+    return step([&](double u){ return filter.filtering(u); }, filter.Ts(), time, gain);
 }
 
 /**
  * @brief 任意の入力による応答を求める
  * 
  * @param sys 
+ * @param dt 
  * @param input 
  * @return std::tuple<std::vector<double>, std::vector<double>> 
  */
-static std::tuple<std::vector<double>, std::vector<double>> lsim(TransferFunction &sys, std::vector<double> input)
+static std::tuple<std::vector<double>, std::vector<double>> lsim(const std::function<double(double)> &sys, double dt, const std::vector<double> &input)
 {
-    std::vector<double> t = linspace(0, sys.Ts()*input.size(), input.size());
+    std::vector<double> t = linspace(0, dt*input.size(), input.size());
     std::vector<double> res(t.size());
-
     for(size_t i = 0; i < res.size(); i++)
     {
-        res[i] = sys.responce(input[i]);
+        res[i] = sys(input[i]);
     }
-
     return {t, res};
 }
+
+template<class SysType, typename std::enable_if<internal::is_system_class<SysType>::value>::type* = nullptr>
+static std::tuple<std::vector<double>, std::vector<double>> lsim(SysType &sys, std::vector<double> input)
+{
+    return lsim([&](double u){ return sys.responce(u); }, sys.Ts(), input);
+}
+
+template<class SysType, typename std::enable_if<internal::is_pure_filter_class<SysType>::value>::type* = nullptr>
+static std::tuple<std::vector<double>, std::vector<double>> lsim(SysType &sys, std::vector<double> input)
+{
+    return lsim([&](double u){ return sys.filtering(u); }, sys.Ts(), input);
+}
+
 
 }
---
+++ test/system/time_responce_test.cpp
@@ -0,0 +1,83 @@
+#include <gtest/gtest.h>
+
+#include <cpp_robotics/system/time_responce.hpp>
+#include <cpp_robotics/filter/low_pass_filter.hpp>
+
+class MyAllPassSystem
+{
+public:
+    double responce(double u)
+    {
+        return u;
+    }
+
+    double Ts() const { return 1e-3; }
+};
+
+class MyAllPassFilter
+{
+public:
+    double filtering(double u)
+    {
+        return u;
+    }
+
+    double Ts() const { return 1e-3; }
+};
+
+TEST(time_responce, impluce) {
+    namespace cr = cpp_robotics;
+
+    {
+        MyAllPassSystem system;
+        auto [t, res] = cr::impluse(system, 1.0);
+    }
+
+    {
+        MyAllPassFilter filter;
+        auto [t, res] = cr::impluse(filter, 1.0);
+    }
+
+    {
+        cr::LowPassFilter filter(10*2*M_PI, 1e-3);
+        auto [t, res] = cr::impluse(filter, 1.0);
+    }
+}
+
+TEST(time_responce, step) {
+    namespace cr = cpp_robotics;
+
+    {
+        MyAllPassSystem system;
+        auto [t, res] = cr::step(system, 1.0);
+    }
+
+    {
+        MyAllPassFilter filter;
+        auto [t, res] = cr::step(filter, 1.0);
+    }
+
+    {
+        cr::LowPassFilter filter(10*2*M_PI, 1e-3);
+        auto [t, res] = cr::step(filter, 1.0);
+    }
+}
+
+TEST(time_responce, lsim) {
+    namespace cr = cpp_robotics;
+
+    {
+        MyAllPassSystem system;
+        auto [t, res] = cr::lsim(system, cr::arrange(0, 3.0, system.Ts()));
+    }
+
+    {
+        MyAllPassFilter filter;
+        auto [t, res] = cr::lsim(filter, cr::linspace(0, 3.0, filter.Ts()));
+    }
+
+    {
+        cr::LowPassFilter filter(10*2*M_PI, 1e-3);
+        auto [t, res] = cr::lsim(filter, cr::linspace(0, 3.0, filter.Ts()));
+    }
+}
